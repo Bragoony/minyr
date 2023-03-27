794f6b48c099db88be57dd7063028b53eb688b3c
Bragoony <bragebigrude@gmail.com>
2023-03-27 14:00:46 +0000

    Gud bedre

--- yr/yr.go
+++ yr/yr.go
@@ -3,8 +3,8 @@ package yr
 import (
 	"fmt"
 	"strconv"
-	//"strings"
-	//"errors"
+	"strings"
+	"errors"
 	"github.com/Bragoony/funtemps/conv"
 )
 
@@ -12,7 +12,7 @@ func CelsiusToFahrenheitString(celsius string) (string, error) {
 	var fahrFloat float64
 	var err error
 	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
-		fahrFloat = conv.CelsiusToFahrenheit(celsiusFloat)
+		fahrFloat = conv.CelsiusToFarhenheit(celsiusFloat)
 	}
 	fahrString := fmt.Sprintf("%.1f", fahrFloat)
 	return fahrString, err
@@ -35,7 +35,7 @@ func CelsiusToFahrenheitLine(line string) (string, error) {
 		return "", errors.New("linje har ikke forventet format")
 	}
 	return strings.Join(dividedString, ";"), nil
-	*/
-	
-	return "Kjevik;SN39040;18.03.2022 01:50;42.8", err
+
+
+	//return "Kjevik;SN39040;18.03.2022 01:50;42.8", err
 }
