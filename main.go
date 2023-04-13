c492e69d7f055ebcf255c6d9079957626685d300
Brage <bragebigrude@gmail.com>
2023-04-13 13:26:53 +0200

    La til alle trengte tester

--- main.go
+++ main.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	//"io"
-	"log"
-	"os"
+	//"log"
+	//"os"
 	//"strings"
-	"bufio"
+	//"bufio"
 
 	"github.com/Bragoony/minyr/yr"
 )
 
 
 func main() {
-	src, err := os.Open("table.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer src.Close()
-	dest, err := os.OpenFile("kjevik-temp-fahr-20220318-20230318.csv", os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dest.Close()
-
-	lineNumber := 0
-
-	scanner := bufio.NewScanner(bufio.NewReader(src))
-	writer := bufio.NewWriter(dest)
-
-	for scanner.Scan(){
-		lineNumber++
-		line := scanner.Text()
-		if lineNumber == 1 {
-			_, err = writer.WriteString(line + "\n")
-			if err != nil {
-				log.Fatal(err)
-				}
-			continue
-			}
-			if lineNumber == 27 {
-				_, err = writer.WriteString(line)
-				if err != nil {
-					log.Fatal(err)
-					}
-				continue
-				}
-			newLine, err := yr.CelsiusToFahrenheitLine(line)
-			_, err = writer.WriteString(newLine + "\n")
-			if err != nil {
-				log.Fatal(err)
-				}
-	}
-	err = writer.Flush()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-
-/*
-func main() {
-	filePath := "table.csv"
+	filePath := "kjevik-temp-celsius-20220318-20230318.csv"
 
 	avg, err := yr.CalculateAverageFourthElement(filePath)
 	if err != nil {
@@ -70,6 +21,5 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Average of fourth elements: %.2f\n", avg)
+	fmt.Printf("Average of fourth elements: %f\n", avg)
 }
-*/
